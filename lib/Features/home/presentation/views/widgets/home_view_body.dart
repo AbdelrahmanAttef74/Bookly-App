4b91b2b3abc5274dade67cbc752ec1017dc15876
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-05-05 15:27:27 +0300

    best seller list view done

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-import 'best_seller_list_item.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -9,19 +9,39 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedListView(),
-          SizedBox(height: 50),
-          Text('Best Seller', style: Styles.testStyle20),
-          SizedBox(height: 20),
-          BestSellerListViewItem(),
-        ],
-      ),
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                child: CustomAppBar(),
+              ),
+              FeaturedListView(),
+              SizedBox(height: 50),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                child: Text('Best Seller', style: Styles.testStyle20),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+        SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 30,
+            ),
+            child: BestSellerListView(),
+          ),
+        )
+      ],
     );
   }
 }
