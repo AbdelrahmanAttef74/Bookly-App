1bc9407e2ff1fd91c59866a498f8040b4a004e99
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-05-09 14:28:19 +0300

    create server failure from dio exeption

--- lib/Features/home/Data/repos/home_repo_imp.dart
+++ lib/Features/home/Data/repos/home_repo_imp.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/Data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImp implements HomeRepo {
   final ApiService apiService;
@@ -21,7 +22,10 @@ class HomeRepoImp implements HomeRepo {
       }
       return right(books);
     } on Exception catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioExeption(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,38 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errorMessage;
+
+  const Failure(this.errorMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errorMessage);
+
+  factory ServerFailure.fromDioExeption(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('connection time out');
+      case DioExceptionType.sendTimeout:
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive time out');
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad certificate');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure('Bad response');
+
+      case DioExceptionType.cancel:
+        return ServerFailure('Request canceld');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure('connection time out');
+
+      case DioExceptionType.unknown:
+        return ServerFailure('Request unknown');
+      default:
+        return ServerFailure('Oops there was ann error');
+    }
+  }
+}
