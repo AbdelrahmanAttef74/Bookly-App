fa3cc07e4024e14e0c08c16b9c436a48dc734c1a
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-07-28 16:58:06 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
